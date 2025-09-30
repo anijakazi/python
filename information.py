9a4d130a723af8542d3e95e618925d9e976700f3
anijakazi <anijakazi926@gmail.com>
2025-09-30 22:28:15 +0530

    first commit

---
+++ information.py
@@ -0,0 +1,46 @@
+# write the program to print ypur age,name,clg name
+#name=input("enter your name:")
+#age=int(input("enter your age:"))
+#collage=input("enter your clg name:")
+
+
+# take two no from user and print their sum,difference,product,division and modual
+
+"""x=int(input("enter the  x value "))
+y=int(input("enter the  y value "))
+print("addition is:",x+y)
+print("difference is:",x-y)
+print("multipation is:",x*y)
+print("division is:",x/y)
+print("modulas is:",x%y)
+
+
+
+
+#calculatr the area and perimeter of rectanglr
+lenght=int(input("enter the value of length:"))
+hight= int(input("enter the value of hight:"))
+
+area=print("area is:",lenght*hight)
+perimeter=print("perimeter is :",2*(lenght+hight))"""
+
+
+
+
+# calculate the area & circumference
+
+radius=float(input("enter the value of radius:"))
+area=3.14*radius*radius
+print(area)
+
+circumference=2*3.14*radius
+print(circumference)
+
+# Write a program to swap two numbers (with and without using a third variable)
+""""x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+x=x+y
+y=x-y
+x=x-y
+print(x)
+print(y)"""
---
+++ pythonclass.py
@@ -0,0 +1,14 @@
+a=int(input("enter the value of a:"))
+print("addition is",a+10)
+print("sub is  ",a-10)
+print("multipation is ",a*10)
+print("division is ",a/2)
+print("reminder is ",a%10)
+print(a//10)
+x=10
+y=20
+x=x+y
+y=x-y
+x=x-y
+print("value of x is:",x)
+print("value of y is :",y)
