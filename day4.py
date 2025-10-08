7487779219a82452e2f9417bfc94b209b20b38c1
anijakazi <anijakazi926@gmail.com>
2025-10-08 07:19:53 +0530

    day4 file add

---
+++ day4.py
@@ -0,0 +1,24 @@
+"""#remove duplcation in string
+
+s="aabbccddeez"
+f=""
+x=""
+for i in s:
+    if i not in f:
+        f=f+i 
+           
+print(f) """
+
+
+
+
+'''s="aabb cc dde ff"
+print(s.replace(" ",""))
+
+for i in range():'''
+
+
+s="anijakazi"
+x=(s.count)
+print(x)
+"""for i in range()""" 
